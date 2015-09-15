1d0b45c1c3edea91925f4daa5f8fca59448b5393
elizabethfong <elizabethfongwm@gmail.com>
2015-09-15 18:35:55 -0400

    SW - alignment complete

--- src/SmithWaterman.java
+++ src/SmithWaterman.java
@@ -1,169 +1,343 @@
 import java.lang.StringBuilder ;
+
+import java.util.ArrayList ;
 import java.util.Stack ;
 
-/**
- * Non-parallel Smith-Waterman
- * 
- * @author Elizabeth Fong
- * @version September 2015, Insight Data Engineering Program NY
- */
-public class SmithWaterman
+
+public class SmithWaterman 
 {
-	public static final int MATCH = 2 ;
-	public static final int MISMATCH = -1 ;
-	public static final int GAP = -1 ;
-	
 	private final String _seq1 , _seq2 ;
 	
-	private int[][] _matrix ;
+	private Proclet[][] _matrix ;
 	
-	/* --- CONSTRUCTION ---------------------------------------------------- */
+	private Alignment[] _optAlignments ;
 	
-	/**
-	 * Initialisation
-	 * 
-	 * @param seq1
-	 * @param seq2
-	 */
+	
 	public SmithWaterman( String seq1 , String seq2 )
 	{
 		_seq1 = seq2 ;
 		_seq2 = seq1 ;
 		
-		// i -> east-directed
-		// j -> south-directed
-		_matrix = new int[seq1.length()+1][seq2.length()+1] ;
+		_matrix = new Proclet[_seq1.length()+1][_seq2.length()+1] ;
 		
-		// init matrix
+		// init top and left edges with 0
 		for( int i = 0 ; i < _matrix.length ; i++ )
-			for( int j = 0 ; j < _matrix[i].length ; j++ )
-				_matrix[i][j] = 0 ;
+			_matrix[i][0] = new Proclet() ;
 		
-		// score matrix
-		for( int i = 1 ; i < _matrix.length ; i ++ )
-			for( int j = 1 ; j < _matrix[i].length ; j++ )
-				_matrix[i][j] = score(i,j) ;
+		for( int j = 1 ; j < _matrix[0].length ; j++)
+			_matrix[0][j] = new Proclet() ;
+		
+		
+		// scoring - returns starting points for finding optimal sequences
+		Stack<Proclet> proclets = scoring() ;
+		
+		
+		// get optimal alignment(s)
+		ArrayList<Alignment> alignments = new ArrayList<Alignment>(proclets.size()) ;
+		
+		int maxScore = 0 ;
+		
+		while( ! proclets.isEmpty() )
+		{
+			Alignment align = getAlignment( proclets.pop() ) ;
+			
+			if( align.score > maxScore )
+			{
+				alignments.clear() ;
+				alignments.add( align ) ;
+				
+				maxScore = align.score ;
+			}
+			else if( align.score == maxScore )
+			{
+				alignments.add( align ) ;
+			}
+		}
+		
+		Alignment[] array = new Alignment[alignments.size()] ;
+		_optAlignments = alignments.toArray( array ) ;
 	}
 	
-	
-	/* --- SCORING --------------------------------------------------------- */
-	
-	// SCORE
-	public int score( int i , int j )
+	private Stack<Proclet> scoring()
 	{
-		char base1 = Character.toUpperCase( _seq1.charAt(i-1) ) ;
-		char base2 = Character.toUpperCase( _seq2.charAt(j-1) ) ;
-		
-		int[] scores = new int[3] ;
-		
-		scores[0] = alignmentScore( i , j , base1 , base2 ) ;
-		scores[1] = insertionScore(i,j) ;
-		scores[2] = deletionScore(i,j) ;
+		// init alignment
+		Stack<Proclet> maxProclets = new Stack<Proclet>() ;
+		maxProclets.push( _matrix[0][0] ) ;
 		
 		int max = 0 ;
 		
-		for( int x = 0 ; x < scores.length ; x++ )
+		// score rest of matrix
+		for( int i = 1 ; i < _matrix.length ; i++ )
 		{
-			int score = scores[x] ;
-			
-			if( score > max )
-				max = score ;
+			for( int j = 1 ; j < _matrix[i].length ; j++ )
+			{
+				Proclet proclet = new Proclet(i,j) ;
+				
+				// proclet score > max score - new max score
+				if( proclet.score > max )
+				{
+					maxProclets = new Stack<Proclet>() ;
+					maxProclets.push( proclet ) ;
+					
+					max = proclet.score ;
+				}
+				
+				// proclet score = max score -> new start pt for opt sequence
+				else if( proclet.score == max )
+				{
+					maxProclets.push( proclet ) ;
+				}
+				
+				_matrix[i][j] = proclet ;
+			}
 		}
 		
-		return max ;
+		return maxProclets ;
 	}
 	
-	// score: alignment -> NW
-	public int alignmentScore( int i , int j , char base1 , char base2 )
+	private Alignment getAlignment( Proclet proc )
 	{
-		return _matrix[i-1][j-1] + similarity( base1 , base2 ) ;
+		// find optimal alignment - push proclets of highest score onto stack
+		Stack<Proclet> proclets = new Stack<Proclet>() ;
+		proclets.push( proc ) ;
+		
+		Proclet next = getNextProclet( proc ) ;
+		
+		while( next.score > 0 )
+		{
+			proclets.push( next ) ;
+			next = getNextProclet( next ) ;
+		}
+		
+		
+		// pop stack to get optimal alignment
+		StringBuilder seq1 = new StringBuilder() ;
+		StringBuilder seq2 = new StringBuilder() ;
+		
+		int alignScore = 0 ;
+		
+		while( ! proclets.isEmpty() )
+		{
+			Proclet tmp = proclets.pop() ;
+			
+			alignScore += tmp.alignmentScore ;
+			
+			seq1.append( tmp.base1 ) ;
+			seq2.append( tmp.base2 ) ;
+		}
+		
+		return new Alignment( seq1.toString() , seq2.toString() , alignScore ) ;
 	}
 	
-	public int similarity( char base1 , char base2 )
+	private Proclet getNextProclet( Proclet proc )
 	{
-		// match
-		if( base1 == base2 )
-			return MATCH ;
-		// mismatch
-		else
-			return MISMATCH ;
+		char alignment = proc.alignmentType ;
+		
+		int i = proc.i ;
+		int j = proc.j ;
+		
+		switch( alignment )
+		{
+			case Proclet.ALIGN :
+				return _matrix[i-1][j-1] ;
+			
+			case Proclet.INSERTION :
+				return _matrix[i][j-1] ;
+			
+			default :
+				return _matrix[i-1][j] ;
+		}
 	}
 	
-	// score: insertion -> north
-	public int insertionScore( int i , int j )
+	private class Proclet
 	{
-		return _matrix[i][j-1] + GAP ;
+		private static final int MATCH = 5 ;
+		private static final int MISMATCH = -3 ;
+		private static final int GAP = -4 ;
+		
+		private static final char ALIGN = 'a' ;
+		private static final char INSERTION = 'i' ;
+		private static final char DELETION = 'd' ;
+		
+		private static final char GAP_CHAR = '_' ;
+		
+		private final int i , j ;
+		
+		private int score ;
+		
+		private char base1 , base2 ;
+		
+		private char alignmentType ;
+		private int alignmentScore ;
+		
+		private Proclet()
+		{
+			i = 0 ;
+			j = 0 ;
+			score = 0 ;
+		}
+		
+		private Proclet( int i , int j )
+		{
+			this.i = i ;
+			this.j = j ;
+			
+			base1 = Character.toUpperCase(_seq1.charAt(i-1)) ;
+			base2 = Character.toUpperCase(_seq2.charAt(j-1)) ;
+			
+			score = procletScore() ;
+			getAlignment() ;
+		}
+		
+		private int procletScore()
+		{
+			int max = 0 ;
+			
+			int tmp = deletionScore() ;
+			if( tmp >= max )
+			{
+				max = tmp ;
+				alignmentType = DELETION ;
+			}
+			
+			tmp = insertionScore() ;
+			if( tmp >= max )
+			{
+				max = tmp ;
+				alignmentType = INSERTION ;
+			}
+			
+			tmp = alignmentScore() ;
+			if( tmp >= max )
+			{
+				max = tmp ;
+				alignmentType = ALIGN ;
+			}
+			
+			return max ;
+		}
+		
+		private int alignmentScore()
+		{
+			int similarity = 0 ;
+			
+			// match or mismatch
+			if( base1 == base2 )
+				similarity = MATCH ;
+			else
+				similarity = MISMATCH ;
+			
+			return _matrix[i-1][j-1].score + similarity ;
+		}
+		
+		// score: insertion -> north
+		private int insertionScore()
+		{
+			return _matrix[i][j-1].score + GAP ;
+		}
+		
+		// score: deletion -> west
+		private int deletionScore()
+		{
+			return _matrix[i-1][j].score + GAP ;
+		}
+		
+		private void getAlignment()
+		{
+			switch( alignmentType )
+			{
+				case ALIGN :
+					if( base1 == base2 )
+						alignmentScore = MATCH ;
+					else
+						alignmentScore = MISMATCH ;
+					break ;
+					
+				case INSERTION :
+					alignmentScore = GAP ;
+					base1 = GAP_CHAR ;
+					break ;
+				
+				case DELETION :
+					alignmentScore = GAP ;
+					base2 = GAP_CHAR ;
+					break ;
+			}
+		}
 	}
 	
-	// score: deletion -> west
-	public int deletionScore( int i , int j )
+	private class Alignment
 	{
-		return _matrix[i-1][j] + GAP ;
+		private String seq1 , seq2 ;
+		private int score ;
+		
+		private Alignment( String seq1 , String seq2 , int alignmentScore )
+		{
+			this.seq1 = seq1 ;
+			this.seq2 = seq2 ;
+			score = alignmentScore ;
+		}
 	}
 	
 	
-	/* --- ALIGNMENT ------------------------------------------------------- */
-	
-	
-	
-	
-	/* --- PRINT ----------------------------------------------------------- */
-	
-	// PRINT MATRIX
-	public String printMatrix()
+	@Override
+	public String toString()
 	{
+		String newline = System.lineSeparator() ;
+		
+		// print matrix
 		StringBuilder str = new StringBuilder() ;
+		str.append( "   _  " ) ;
+		
+		for( int i = 0 ; i < _seq2.length() ; i++ )
+		{
+			str.append( Character.toUpperCase(_seq2.charAt(i)) + "  " ) ;
+		}
+		
+		str.append( newline + newline ) ;
 		
 		for( int i = 0 ; i < _matrix.length ; i++ )
 		{
-			for( int j = 0 ; j < _matrix[i].length ; j++ )
-				str.append( _matrix[i][j] + " " ) ;
+			if( i == 0 )
+				str.append( "_  " ) ;
+			else
+				str.append( Character.toUpperCase(_seq1.charAt(i-1)) + "  " ) ;
 			
-			str.append( System.lineSeparator() ) ;
+			for( int j = 0 ; j < _matrix[i].length ; j++ )
+			{
+				int score = _matrix[i][j].score ;
+				
+				if( score < 10 )
+					str.append( score + "  " ) ;
+				else
+					str.append( score + " " ) ;
+			}
+			
+			str.append( newline + newline ) ;
+		}
+		
+		// optimal alignment(s)
+		for( int i = 0 ; i < _optAlignments.length ; i++ )
+		{
+			Alignment align = _optAlignments[i] ;
+			
+			str.append( "total score = " + align.score + newline ) ;
+			str.append( align.seq2 + newline ) ;
+			str.append( align.seq1 + newline ) ;
+			str.append( newline ) ;
 		}
 		
 		return str.toString() ;
 	}
 	
-	
-	/* --- FOR STACK ------------------------------------------------------- */
-	
-	private class Node
-	{
-		private static final int ALIGN = 0 ;
-		private static final int INSERT = 1 ;
-		private static final int DELETE = 2 ;
-		
-		private final char base1 , base2 ;
-		private final int i , j ;
-		private final int score ;
-		private int alignType ;
-		
-		private Node( char base1 , char base2 , int i , int j , int score )
-		{
-			this.base1 = base1 ;
-			this.base2 = base2 ;
-			this.i = i ;
-			this.j = j ;
-			this.score = score ;
-		}
-		
-		private void setAlignType( int alignType )
-		{
-			this.alignType = alignType ; 
-		}
-	}
-	
-	
-	/* --- MAIN ------------------------------------------------------------ */
-	
-	// MAIN
 	public static void main( String[] args )
 	{
 		//String str1 = "ACACACTA" ;
 		//String str2 = "AGCACACA" ;
-		String str1 = "ATGCA" ;
-		String str2 = "ACTCA" ;
+		//String str1 = "ATGCA" ;
+		//String str2 = "ACTCA" ;
+		String str1 = "CGTGAATTCAT" ;
+		String str2 = "GACTTAC" ;
 		
 		System.out.println( "Str1 = " + str1 ) ;
 		System.out.println( "Str2 = " + str2 ) ;
@@ -171,6 +345,6 @@ public class SmithWaterman
 		SmithWaterman sw = new SmithWaterman(str1,str2) ;
 		
 		System.out.println( "" ) ;
-		System.out.println( sw.printMatrix() ) ;
+		System.out.println( sw.toString() ) ;
 	}
 }
