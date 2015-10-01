32ea047f5126899fa9e211fa963a512b1173e60a
elizabethfong <elizabethfongwm@gmail.com>
2015-10-01 03:53:00 -0400

    moved existing file operations to InOutOps.java

--- src/com/sw/Alignment.java
+++ src/com/sw/Alignment.java
@@ -2,5 +2,35 @@ package com.sw ;
 
 public class Alignment 
 {
+	// CONSTANTS
+	final String FILE_REF = "/home/ubuntu/project/testRef/vertebrate_mammalian.107.rna.fna" ;
+	final String FILE_IN = "/home/ubuntu/project/testIn/test5.fa" ;
 	
+	final int[] ALIGN_SCORES = {5,-3,-4} ;	// {match,mismatch,gap}
+	final char[] ALIGN_TYPES = {'a','i','d','-'} ;
+	final String DELIMITER = ">gi" ;
+	
+	final String APP_NAME = "Smith-Waterman in Spark" ;
+	
+	
+	public static void main( String[] args )
+	{
+		// TODO main - code here
+	}
+	
+	
+	// DISTRIBUTE ALGORITHM
+	
+	
+	// DISTRIBUTE REFERENCE
+	
+	
+	// DISTRIBUTE READS
+	
+	
+	// NO DISTRIBUTION
+	public static class NoDistribution
+	{
+		
+	}
 }
--- src/com/sw/InOutOps.java
+++ src/com/sw/InOutOps.java
@@ -6,20 +6,152 @@ import java.io.FileNotFoundException ;
 import java.io.FileWriter ;
 import java.io.IOException ;
 
+import java.util.ArrayList ;
 import java.util.Scanner ;
 
-import org.apache.spark.api.java.function.VoidFunction ;
+import org.apache.spark.api.java.function.Function2 ;
+
 
 @SuppressWarnings( "serial" )
 public class InOutOps
 {
-	public static class PrintToFile implements VoidFunction<String>
+	public static final String NEWLINE = System.lineSeparator() ;
+	
+	
+	/* --- INPUT ----------------------------------------------------------- */
+	
+	public static class GetReads implements Function2< String , String , ArrayList<String> >
 	{
-		public void call( String data )
+		public ArrayList<String> call( String filepath , String delimiter )
 		{
+			ArrayList<String> reads = new ArrayList<String>() ;
 			
+			try
+			{
+				Scanner scanner = new Scanner( new File(filepath) ) ;
+				
+				// first line - may contain metadata
+				String line = scanner.nextLine().trim() ;
+				
+				if( ! new IsMetadata().call(line,delimiter).booleanValue() )
+					reads.add( line ) ;
+				
+				// subsequent lines
+				while( scanner.hasNextLine() )
+					reads.add( scanner.nextLine().trim() ) ;
+				
+				// done!
+				scanner.close() ;
+			}
+			catch( FileNotFoundException fnfe )
+			{
+				System.out.println( "Input file not found." ) ;
+				System.exit(0) ;
+			}
+			
+			return reads ;
+		}
+	}
+	
+	public static class GetRefSeqs implements Function2< String , String , ArrayList<String[]> >
+	{
+		public ArrayList<String[]> call( String filepath , String delimiter )
+		{
+			ArrayList<String[]> sequences = new ArrayList<String[]>() ;
+			
+			try
+			{
+				Scanner scanner = new Scanner( new File(filepath) ) ;
+				
+				String[] ref = null ;
+				StringBuilder seq = null ;
+				
+				// each line
+				while( scanner.hasNextLine() )
+				{
+					String line = scanner.nextLine() ;
+					
+					if( new IsMetadata().call(line,delimiter).booleanValue() )
+					{
+						if( ref != null )
+						{
+							ref[1] = seq.toString() ;
+							sequences.add(ref) ;
+							
+							seq = null ;
+							ref = null ;
+						}
+						
+						ref = new String[2] ;
+						ref[0] = line ;
+						seq = new StringBuilder() ;
+					}
+					else
+					{
+						seq.append( line ) ;
+					}
+				}
+				
+				// last line
+				ref[1] = seq.toString() ;
+				sequences.add(ref) ;
+				
+				seq = null ;
+				ref = null ;
+				
+				scanner.close() ;
+			}
+			catch( FileNotFoundException fnfe )
+			{
+				System.out.println( "Reference file not found" ) ;
+				System.exit(0) ;
+			}
+			
+			return sequences ;
 		}
 	}
 	
 	
+	/* --- OUTPUT ---------------------------------------------------------- */
+	
+	public static class PrintToFile implements Function2< String , String , Boolean >
+	{
+		public Boolean call( String filepath , String data )
+		{
+			try
+			{
+				File file = new File( filepath ) ;
+				
+				if( ! file.exists() )
+					file.createNewFile() ;
+				
+				BufferedWriter writer = new BufferedWriter( new FileWriter(file.getAbsolutePath(),false) ) ;
+				
+				writer.write( data ) ;
+				writer.close() ;
+				
+				return true ;
+			}
+			catch( IOException ioe )
+			{
+				System.out.println( "IOException on writing to file" ) ;
+				ioe.printStackTrace() ;
+				return false ;
+			}
+		}
+	}
+	
+	
+	/* --- UTILITY --------------------------------------------------------- */
+	
+	private static class IsMetadata implements Function2< String , String , Boolean >
+	{
+		public Boolean call( String line , String delimiter )
+		{
+			if( line.length() >= delimiter.length() && line.substring(0,delimiter.length()).equals(delimiter) )
+				return true ;
+			else
+				return false ;
+		}
+	}
 }
