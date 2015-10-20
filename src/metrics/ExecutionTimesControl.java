9171d0ae09a6412e7344f4c42569861b6fc461be
elizabethfong <elizabethfongwm@gmail.com>
2015-10-20 11:34:07 -0400

    complete - metrics: control

--- src/metrics/ExecutionTimesControl.java
+++ src/metrics/ExecutionTimesControl.java
@@ -50,7 +50,7 @@ public class ExecutionTimesControl
 		String outFileExt = null ;
 		
 		String[] args = { refDir , inDir , delimiter , outDir , outFileName , outFileExt } ;
-		new Distribution.DistributeReference().call( args , null ) ;
+		new Distribution.NoDistribution().call( args , null ) ;
 	}
 	
 	/**
@@ -68,7 +68,7 @@ public class ExecutionTimesControl
 		String outFileExt = null ;
 		
 		String[] args = { refDir , inDir , delimiter , outDir , outFileName , outFileExt } ;
-		new Distribution.DistributeReference().call( args , null ) ;
+		new Distribution.NoDistribution().call( args , null ) ;
 	}
 	
 	/**
@@ -93,7 +93,7 @@ public class ExecutionTimesControl
 			String outFileName = outGeneralName + i + "_" ;
 			
 			String[] args = { refDir , inDir , delimiter , outDir , outFileName , outFileExt } ;
-			new Distribution.DistributeReference().call( args , null ) ;
+			new Distribution.NoDistribution().call( args , null ) ;
 		}
 	}
 	
@@ -119,7 +119,7 @@ public class ExecutionTimesControl
 			String outFileName = outGeneralName + i + "_" ;
 			
 			String[] args = { refDir , inDir , delimiter , outDir , outFileName , outFileExt } ;
-			new Distribution.DistributeReference().call( args , null ) ;
+			new Distribution.NoDistribution().call( args , null ) ;
 		}
 	}
 	
